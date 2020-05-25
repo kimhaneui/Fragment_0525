2e13b70071423c61f2abc8057d7488e2b28603c0
kimhaneui <kimhaneui2@naver.com>
2020-05-25 16:38:43 +0900

    프래그먼트 데이터바인딩 + onActivityCreated에서 활용

--- app/src/main/java/com/example/fragment_0525/Fragment/FirstFragment.java
+++ app/src/main/java/com/example/fragment_0525/Fragment/FirstFragment.java
@@ -7,15 +7,34 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.fragment_0525.R;
+import com.example.fragment_0525.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
+
+    FragmentFirstBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first,container,false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_first,container,false);
+        return  binding.getRoot();
     }
+
+//    나를 담고 있는 액티비티의 onCreate가 실행된 시점
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = binding.messageEdt.getText().toString();
+            }
+        });
+    }
+//    액티비티가
+
 }
